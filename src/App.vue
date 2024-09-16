5921daea90ebdaa612f76a91579a11a6bd1af0e0
GuanJiayao <978980697@qq.com>
2024-09-16 18:31:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <ThemeSwitcher />
+    <div class="card flex flex-col md:flex-row gap-4">
+        <InputGroup>
+            <Button label="Search" />
+            <InputText placeholder="Keyword" />
+        </InputGroup>
+
+        <InputGroup>
+            <InputText placeholder="Keyword" />
+            <Button icon="pi pi-search" severity="warn" />
+        </InputGroup>
+
+        <InputGroup>
+            <Button icon="pi pi-check" severity="success" />
+            <InputText placeholder="Vote" />
+            <Button icon="pi pi-times" severity="danger" />
+        </InputGroup>
+    </div>
+</template>
